26c0a73a24ddadb78f3466d41665791a4947e93b
algorithm <94842082+hikehakim@users.noreply.github.com>
2024-05-12 21:52:30 +0800

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        System.out.println("hello world2");
+        System.out.println("hello world 修改分支中ing");
         System.out.println("hello world fix");
+        System.out.println("hello world fix2");
+
     }
 }
